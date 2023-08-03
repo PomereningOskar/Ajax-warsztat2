cc342523d03e33266396a45e74f296118d0fe54d
PomereningOskar <oskar.staskiewicz1@wp.pl>
2023-08-03 19:54:25 +0200

    warsztat 2

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $('#get-data').click(function () {
+
+        //         wariant 1
+        //         $.get('https://akademia108.pl/api/ajax/get-post.php')
+        //             .done(function (data) {
+
+        //                 let pId = $('<p></p').text(`Post ID: ${data.id}`)
+        //                 let pUserId = $('<p></p').text(`Post ID: ${data.userId}`)
+        //                 let pTitle = $('<p></p').text(`Post ID: ${data.title}`)
+        //                 let pBody = $('<p></p').text(`Post ID: ${data.body}`)
+        //                 let hr = $('<hr />')
+
+        //                 let jqBody = $('body').append(pId)
+
+        //                 jqBody.append(pId)
+        //                 jqBody.append(pUserId)
+        //                 jqBody.append(pTitle)
+        //                 jqBody.append(pBody)
+        //                 jqBody.append(hr)
+
+        //                 // console.log(data);
+        //             })
+        //             .fail(function (error) {
+        //                 console.log(error);
+        //             })
+        //     })
+        // })
+
+        //wariant 2
+
+        $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+            .done(function (data) {
+
+                let pId = $('<p></p').text(`Post ID: ${data.id}`)
+                let pUserId = $('<p></p').text(`Post ID: ${data.userId}`)
+                let pTitle = $('<p></p').text(`Post ID: ${data.title}`)
+                let pBody = $('<p></p').text(`Post ID: ${data.body}`)
+                let hr = $('<hr />')
+
+                let jqBody = $('body').append(pId)
+
+                jqBody.append(pId)
+                jqBody.append(pUserId)
+                jqBody.append(pTitle)
+                jqBody.append(pBody)
+                jqBody.append(hr)
+
+                // console.log(data);
+            })
+            .fail(function (error) {
+                console.log(error);
+            })
+    })
+})
